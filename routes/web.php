a9db79b3cce2bc4281ec10b25fa5b7571b65e282
Ilya Sushkov <pyh3@yandex.ru>
2021-05-25 22:43:49 +0300

    update front

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,33 @@ Route::get('/', function () {
 Route::resource('/test', TestController::class);
 
 Route::get('/onePlant', [TestController::class, 'onePlant']) ->name('onePlant');
+
+Route::group([
+    'prefix' => '/plants',
+    'as' => 'admin::',
+    'namespace' => 'App\Http\Controllers\Admin',
+    'middleware' => ['auth']
+], function () {
+    Route::get('/', 'NewsController@index')
+        ->name('news');
+
+    Route::get('/create', 'NewsController@createView')
+        ->name('createView');
+
+    Route::post('/create', 'NewsController@create')
+        ->name('create');
+
+    Route::get('/update/{id}', 'NewsController@updateView')
+        ->name('updateView');
+
+    Route::post('/update/{id}', 'NewsController@update')
+        ->name('update');
+
+    Route::get('/delete/{id}', 'NewsController@delete')
+        ->name('delete');
+
+    Route::get('parser', [ParserController::class, 'index'])
+        ->name('parserNews');
+//    Route::get('parserrr', [ParserController::class, 'currencyExchange'])
+//        ->name('parserCurrency ');
+});
