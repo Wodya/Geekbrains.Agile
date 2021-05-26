66faa91480a7e144b5df7ca794d08b3364566853
Anastasya <bepin@yandex.ru>
2021-05-26 16:31:58 +0400

    table

---
+++ resources/views/plants/chosenTable.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.main')
+@section('content')
+<div class="container container-fluid">
+
+<!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Список избранных растений</h1>
+        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-plus fa-sm text-white-50"></i> Добавить растение</a>
+    </div>
+
+
+    <div class="row">
+    
+    <table class="table table-bordered">
+    <thead>
+    <tr>
+    <th>Название растения</th>
+    <th>Дата добавления</th>
+    <th>Полив</th>
+    <th>Удобрение</th>
+    <th>Обрезка</th>
+    <th>Обработка от вредителей</th>
+    <th>Действия</th>
+    </tr>
+    </thead>
+    <tbody>
+    @forelse($plants as $plant)
+    <tr>
+    <td><a href="{{route('onePlant', ['id' => $plant->id])}}">{{$plant->name}}</a></td>
+    <td>now()</td>
+    <td>полив</td>
+    <td>Удобрение</td>
+    <td>Обрезка</td>
+    <td>Обработка от вредителей</td>
+    <td><a href="#">Удалить из избранного</a></td>
+    </tr>
+    @empty
+    <tr>
+        <td calspan="4"> Выбранных растений нет</td>
+    </tr>
+    @endforelse
+    </tbody></table>
+    </div>
+
+</div>
+
+@endsection
