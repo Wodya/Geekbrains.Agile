6d60068ca1a14ee719ee62fa6cb93fae0427d17c
vagrant <vagrant@homestead>
2021-05-26 18:47:51 +0000

    Переделал контроллеры

---
+++ app/Http/Controllers/PlantsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PlantFull;
+use App\Service\IDbPlantService;
+use Illuminate\Http\Request;
+
+class PlantsController extends Controller
+{
+    public function index(Request $request, IDbPlantService $dbPlant)
+    {
+        $plantList = $dbPlant->getAllPlants();
+        return view('plants.plantsList',['plantsList' => $plantList]);
+    }
+
+    public function onePlant($id, IDbPlantService $dbPlant)
+    {
+        $onePlant = $dbPlant->getPlant($id);
+        // return "<h2>Отобразить запись одного товара с ID = {$id}</h2>";
+
+        // dd($onePlant);
+        return view('plants.onePlant', ['onePlant' => $onePlant]);
+    }
+}
