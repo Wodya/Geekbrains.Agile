d39af1b1a20705e00870f1169d170464dd9ad5f4
Cheglok <kopytovmikhail@yandex.ru>
2021-06-02 18:39:19 +0000

    закончил с грубой стилизацией админки

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -3,26 +3,23 @@
         <ul class="nav" id="main-menu">
 
             <li>
-                <a href="/catalog"><i class="fa fa-dashboard"></i> На главную</a>
+                <a href="{{route('catalog')}}"><i class="fa fa-dashboard"></i>Каталог</a>
             </li>
             <li>
-                <a href=""><i class="fa fa-dashboard"></i> Dashboard</a>
+                <a href="{{route('admin::plants::plantList')}}"><i class="fa fa-dashboard"></i> Список растений</a>
             </li>
             <li>
-                <a href="#"><i class="fa fa-sitemap"></i> Новости<span class="fa arrow"></span></a>
+                <a href="#"><i class="fa fa-sitemap"></i> Выпадающее меню<span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li><p>Базовые категории</p></li>
                     <li>
-                        <a href="#">Категории</a>
+                        <a href="#">Пункт меню</a>
                     </li>
                     <li>
-                        <a href="#">Новости</a>
+                        <a href="#">Пункт меню</a>
                     </li>
                     <li>
-                        <a href="#">Пользователи</a>
-                    </li>
-                    <li>
-                        <a href="#">Формы обратной связи</a>
+                        <a href="#">Пункт меню</a>
                     </li>
                     <li>
                         <a href="#">Дополнительная панель<span class="fa arrow"></span></a>
