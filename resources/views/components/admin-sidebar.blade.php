40c8f2c409a3b09fc5c2b3a67ade9d01071ea5c8
Cheglok <kopytovmikhail@yandex.ru>
2021-06-02 17:52:00 +0000

    в процессе смены темплейта

---
+++ resources/views/components/admin-sidebar.blade.php
@@ -0,0 +1,45 @@
+<nav class="navbar-default navbar-side" role="navigation">
+    <div class="sidebar-collapse">
+        <ul class="nav" id="main-menu">
+
+            <li>
+                <a href="/catalog"><i class="fa fa-dashboard"></i> На главную</a>
+            </li>
+            <li>
+                <a href=""><i class="fa fa-dashboard"></i> Dashboard</a>
+            </li>
+            <li>
+                <a href="#"><i class="fa fa-sitemap"></i> Новости<span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li><p>Базовые категории</p></li>
+                    <li>
+                        <a href="#">Категории</a>
+                    </li>
+                    <li>
+                        <a href="#">Новости</a>
+                    </li>
+                    <li>
+                        <a href="#">Пользователи</a>
+                    </li>
+                    <li>
+                        <a href="#">Формы обратной связи</a>
+                    </li>
+                    <li>
+                        <a href="#">Дополнительная панель<span class="fa arrow"></span></a>
+                        <ul class="nav nav-third-level">
+                            <li>
+                                <a href="#">Здесь будут ещё инструменты</a>
+                            </li>
+                            <li>
+                                <a href="#">Здесь будут ещё инструменты</a>
+                            </li>
+                        </ul>
+
+                    </li>
+                </ul>
+            </li>
+        </ul>
+
+    </div>
+
+</nav>
