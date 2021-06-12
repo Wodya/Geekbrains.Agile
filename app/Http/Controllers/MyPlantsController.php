1fcf8c687f2345f475a46a3ac1d3d9435cae0022
Cheglok <kopytovmikhail@yandex.ru>
2021-06-12 19:07:04 +0000

    немного упорядочил роуты в web.php, сделал предложение зарегистрироваться для пытающихся добавить в избранное вместо алерта, вынес модальные окна в компонент

--- app/Http/Controllers/MyPlantsController.php
+++ app/Http/Controllers/MyPlantsController.php
@@ -8,12 +8,7 @@ use Illuminate\Support\Facades\Redirect;
 
 class MyPlantsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request, IDbPlantService $dbPlant)
+    public function favorPlants(Request $request, IDbPlantService $dbPlant)
     {
         $plants = $dbPlant->getFavorPlants(1);
         return view('plants.chosenTable',['plants' => $plants]);
@@ -25,64 +20,6 @@ class MyPlantsController extends Controller
         echo('$plantId = ' . $plantId);
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param $userId
-     * @param $plantId
-     * @param \Illuminate\Http\Request $request
-     * @param IDbPlantService $dbPlant
-     * @return void
-     */
-    public function store()
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id, IDbPlantService $dbPlant)
-    {
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id, IDbPlantService $dbPlant)
-    {
-
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Request $request, IDbPlantService $dbPlant)
-    {
-      //
-    }
     public function removeFavor($userId, $plantId, Request $request, IDbPlantService $dbPlant)
     {
         $dbPlant->removePlantFromFavor($userId, $plantId);
