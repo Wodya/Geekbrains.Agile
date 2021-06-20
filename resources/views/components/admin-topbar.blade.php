48bff5eccf6e6512edff27027b697bc3f2ba7ddc
Kostin <kostin2008@yandex.ru>
2021-06-20 16:23:52 +0300

    начал работать над личным кабинетом пользователя

--- resources/views/components/admin-topbar.blade.php
+++ resources/views/components/admin-topbar.blade.php
@@ -5,13 +5,12 @@
     </div>
     <ul class="nav navbar-top-links navbar-right">
         <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#" aria-expanded="false">
+            <a class="dropdown-toggle" href="{{route('admin::plants::plantList')}}">
                 {{Auth::user()->name}}
                 <img class="img-profile rounded-circle" src="{{Auth::user()->avatar}}" width="25" height="25" alt="">
-                <i class="fa fa-caret-down"></i>
-
+                <!-- <i class="fa fa-caret-down"></i> -->
             </a>
-            <ul class="dropdown-menu dropdown-user">
+            <!-- <ul class="dropdown-menu dropdown-user">
                 <li><a href="#">
                         <i class="fa fa-user fa-fw"></i>
                         {{Auth::user()->name}}
@@ -22,7 +21,7 @@
                 <li class="divider"></li>
                 <li><a href="#"><i class="fa fa-sign-out fa-fw"></i> Выход</a>
                 </li>
-            </ul>
+            </ul> -->
             <!-- /.dropdown-user -->
         </li>
     </ul>
--- resources/views/components/site-sidebar.blade.php
+++ resources/views/components/site-sidebar.blade.php
@@ -60,15 +60,20 @@
                             <li class="menu-item menu-blog">
                                 <a href="{{route('favorPlants')}}">
                                     <p>Избранные растения</p>
+                                </a>
+                            </li>
+                            <li class="menu-item menu-blog">
+                                <a href="{{route('calendar')}}">Календарь работ</a>
+                            </li>
+                            <li class="menu-item menu-blog">
+                                <a href="{{route('account')}}">
+                                    <p>Личный кабинет</p>
                                     @if(isset(Auth::user()->avatar))
                                     <img class="img-profile rounded-circle " src="{{Auth::user()->avatar}}"
                                         height="40px" width="40px">
                                     @endif
                                     <small class="text-muted text-capitalize">{{ Auth::user()->name }}</small>
                                 </a>
-                            </li>
-                            <li class="menu-item menu-blog">
-                                <a href="{{route('calendar')}}">Календарь работ</a>
                             </li>
                             <li class="menu-item menu-blog">
                                 <a href="{{route('logout')}}">
